5a12fc19cae4d75e955db2c5ef1b2922876a2d4a
youngbin <redmangos@naver.com>
2024-02-15 16:50:27 +0900

    add login with clerk

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      // JWT Templates에 생성된 Template 클릭 후 Issuer URL 넣기
+      domain: "https://literate-sole-54.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
