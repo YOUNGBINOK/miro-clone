c119f3fcb95873ccc4b1872971049baf6d1737bd
youngbin <redmangos@naver.com>
2024-03-04 21:06:21 +0900

    add loading

--- convex/providers/convex-client-provider.tsx
+++ convex/providers/convex-client-provider.tsx
@@ -3,12 +3,15 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { Loading } from "@/components/auth/loading";
+
 import React from "react";
+
+// Authenticated : 인증
 
 interface ConvexClientProviderProps {
   children: React.ReactNode;
 }
-
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
 
 const convex = new ConvexReactClient(convexUrl);
@@ -19,7 +22,10 @@ export const ConvexClientProvider = ({
   return (
     <ClerkProvider>
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-        {children}
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
